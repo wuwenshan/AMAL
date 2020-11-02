a7d91100d980136430a1d4b9520d1097e55abecb
Wenshan <wenshan.wu@saynova.com>
2020-11-02 17:24:32 +0100

    Mise à jour du TME 2 : correction du stochastique + minibatch (ajout aléa)

--- student_tp2/src/tp2_boston.py
+++ student_tp2/src/tp2_boston.py
@@ -4,22 +4,31 @@ from tqdm import tqdm
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 import numpy as np
+import math
+import matplotlib.pyplot as plt
+# import datamaestro
 
 TEST_SIZE = 0.2
-EPS = 1e-3
-EPOCHS = 2000
-BATCH_SIZE = 32
+EPS = 0.01
+EPOCHS = 20    
+BATCH_SIZE = 20
 isNorm = True
-grad_descent = "batch"
+grad_descent = "minibatch"
 
 writer = SummaryWriter()
+
+# data=datamaestro.prepare_dataset("edu.uci.boston")
+# colnames, datax, datay = data.data()
+# datax = torch.tensor(datax,dtype=torch.float)
+# datay = torch.tensor(datay,dtype=torch.float).reshape(-1,1)
 
 X, y = load_boston(return_X_y=True)
 
 y = y.reshape(-1,1)
 
+
 if isNorm:
-    X = ( X - np.mean(X, 0) ) / np.var(X, 0)
+    X = ( X - np.mean(X, 0) ) / np.std(X, 0)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=42)
 
@@ -31,71 +40,98 @@ y_test = torch.from_numpy(y_test).float()
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
 # Les paramètres du modèle à optimiser
-w = torch.randn((X_train.shape[1], 1), requires_grad=True)
+w = torch.normal(mean=0, std=1/math.sqrt(X_train.shape[0]),size=(X_train.shape[1], y_train.shape[1]), requires_grad=True)
 b = torch.randn(1, requires_grad=True)
+
+loss_train_tab = []
+loss_test_tab = []
 
 if grad_descent == "batch":
 
-    for i in range(EPOCHS):
+    for i in tqdm(range(EPOCHS)):
         yhat = torch.mm(X_train, w) + b
     
-        loss = (1/y_train.shape[0]) * torch.pow(torch.norm(yhat - y_train), 2)
-        loss.backward()
+        loss_train = (1/y_train.shape[0]) * torch.pow(torch.norm(yhat - y_train), 2)
+        loss_train.backward()
         
-        print(f"Itérations {i}: batch/loss/train {loss.item()}")
-        writer.add_scalar('batch/loss/train', loss, i)
+        loss_train_tab.append(loss_train)
+        #print(f"Itérations {i}: batch/loss/train {loss.item()}")
+        #writer.add_scalar('batch/loss/train', loss_train, i)
         
-        w.data -= EPS * w.grad.data
-        b.data -= EPS * b.grad.data
+        w.data -= EPS * w.grad
+        b.data -= EPS * b.grad
         
         w.grad.data.zero_()
         b.grad.data.zero_()
         
         with torch.no_grad():
             yhat = torch.mm(X_test, w) + b
-            loss = (1/y_test.shape[0]) * torch.pow(torch.norm(yhat - y_test), 2)
-            writer.add_scalar('batch/loss/test', loss, i)
-            print(f"Itérations {i}: batch/loss/test {loss.item()}")
+            loss_test = (1/y_test.shape[0]) * torch.pow(torch.norm(yhat - y_test), 2)
+            loss_test_tab.append(loss_test)
+            #writer.add_scalar('batch/loss/test', loss, i)
+            #print(f"Itérations {i}: batch/loss/test {loss.item()}")
+            
+        writer.add_scalars('batch/loss', {'loss train' : loss_train, 'loss test' : loss_test}, i)
             
 elif grad_descent == "stochastic":
-    idx = torch.randperm(len(X_train))
-    t = 0
     
-    for i in range(EPOCHS):
-        yhat = torch.mm(X_train[idx[t]].reshape(1,-1), w) + b
+    for i in tqdm(range(EPOCHS)):
         
-        loss = (1/y_train[idx[t]].shape[0]) * torch.pow(torch.norm(yhat - y_train[idx[t]].reshape(1,-1)), 2)
-        loss.backward()
+        cumul_loss = 0
+        t = 0
         
-        print(f"Itérations {i}: stoch/loss/train {loss.item()}")
-        writer.add_scalar('stoch/loss/train', loss, i)
+        for _ in range(X_train.shape[0]):
+            
+            j = np.random.randint(X_train.shape[0])
+            yhat = torch.mm(X_train[j].reshape(1,-1), w) + b
+            
+            loss_train = (1/y_train[j].shape[0]) * torch.pow(torch.norm(yhat - y_train[j].reshape(1,-1)), 2)
+            loss_train.backward()
+            
+            #print(f"Itérations {i}: stoch/loss/train {loss_train.item()}")
+            #writer.add_scalar('Test/stoch/loss/train', loss_train, i)
+            
+            cumul_loss += loss_train.item()
+            
+            w.data -= EPS * w.grad.data
+            b.data -= EPS * b.grad.data
+            
+            
+            w.grad.data.zero_()
+            b.grad.data.zero_()
         
-        w.data -= EPS * w.grad.data
-        b.data -= EPS * b.grad.data
-        
-        w.grad.data.zero_()
-        b.grad.data.zero_()
-        
-        t += 1
+            t += 1
+            
+        loss_train_tab.append(cumul_loss / t)
         
         with torch.no_grad():
             yhat = torch.mm(X_test, w) + b
-            loss = (1/y_test.shape[0]) * torch.pow(torch.norm(yhat - y_test), 2)
-            writer.add_scalar('stoch/loss/test', loss, i)
-            print(f"Itérations {i}: stoch/loss/test {loss.item()}")
+            loss_test = (1/y_test.shape[0]) * torch.pow(torch.norm(yhat - y_test), 2)
+            loss_test_tab.append(loss_test)
+            #writer.add_scalar('stoch/loss/test', loss, i)
+            #print(f"Itérations {i}: stoch/loss/test {loss_test.item()}")
+    
+        writer.add_scalars('stoch/loss', {'loss train' : cumul_loss / t, 'loss test' : loss_test}, i)
     
 elif grad_descent == "minibatch":
     
+    ind = np.arange(0, X_train.shape[0])
+    np.random.shuffle(ind)
+    
+    X_train = X_train[ind]
+    y_train = y_train[ind]
+    
     for i in range(EPOCHS):
         cumul_loss = 0
+        c = 0
         for j in range(0, len(X_train) - BATCH_SIZE, BATCH_SIZE):
-        
+            c += 1
             yhat = torch.mm(X_train[j:j+BATCH_SIZE], w) + b
         
             loss = (1/y_train[j:j+BATCH_SIZE].shape[0]) * torch.pow(torch.norm(yhat - y_train[j:j+BATCH_SIZE]), 2)
             loss.backward()
             
-            print(f"Itérations {i}: minib/loss/train {loss.item()}")
+            #print(f"Itérations {i}: minib/loss/train {loss.item()}")
             cumul_loss += loss.item()
             
             w.data -= EPS * w.grad.data
@@ -104,10 +140,24 @@ elif grad_descent == "minibatch":
             w.grad.data.zero_()
             b.grad.data.zero_()
             
-        writer.add_scalar('minib/loss/train', cumul_loss, i)
+        #writer.add_scalar('minib/loss/train', cumul_loss, i)
             
+        loss_train_tab.append(cumul_loss / c)
         with torch.no_grad():
             yhat = torch.mm(X_test, w) + b
             loss = (1/y_test.shape[0]) * torch.pow(torch.norm(yhat - y_test), 2)
-            print(f"Itérations {i}: minib/loss/test {loss.item()}")
-            writer.add_scalar('minib/loss/test', loss, i)
+            loss_test_tab.append(loss)
+            #print(f"Itérations {i}: minib/loss/test {loss.item()}")
+            #writer.add_scalar('minib/loss/test', loss, i)
+            
+        writer.add_scalars('minib/loss', {'loss_train' : cumul_loss/c, 'loss_test' : loss}, i)
+        
+        
+if len(loss_train_tab) > 0 and len(loss_test_tab) > 0:
+    fig, ax = plt.subplots(figsize=(10,5))
+    ax.grid()
+    ax.set(xlabel = "Nombre d'epochs", ylabel="Loss", title=f"Evolution de la loss sur {len(loss_train_tab)} epochs")
+    plt.plot(np.arange(len(loss_train_tab)), loss_train_tab, label="train")
+    plt.plot(np.arange(len(loss_test_tab)), loss_test_tab, label="test")
+    ax.legend()
+    plt.savefig(f"loss_boston_{grad_descent}")   
--- student_tp2/src/tp2_nn_boston.py
+++ student_tp2/src/tp2_nn_boston.py
@@ -4,6 +4,10 @@ from tqdm import tqdm
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 import numpy as np
+import time
+from datetime import datetime
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 class NN(torch.nn.Module):
@@ -29,12 +33,12 @@ def NN_conteneur(x_size, h_size, out_size):
     return model, criterion, optim
     
 TEST_SIZE = 0.2
-EPS = 1e-2
-EPOCHS = 100
+EPS = 0.01
+EPOCHS = 50
 BATCH_SIZE = 32
 H_SIZE = 128
 isNorm = True
-grad_descent = "batch"
+grad_descent = "minibatch"
 
 writer = SummaryWriter()
 
@@ -43,7 +47,7 @@ X, y = load_boston(return_X_y=True)
 y = y.reshape(-1,1)
 
 if isNorm:
-    X = ( X - np.mean(X, 0) ) / np.var(X, 0)
+    X = ( X - np.mean(X, 0) ) / np.std(X, 0)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=42)
 
@@ -60,19 +64,25 @@ optimizer = torch.optim.Adam(model.parameters(),lr=EPS)
 criterion = torch.nn.MSELoss()
 """
 
+loss_train_tab = []
+loss_test_tab = []
+
 model, criterion, optimizer = NN_conteneur(X.shape[1], H_SIZE, 1)
 
 if grad_descent == "batch":
-
+    
+    tps = time.time()
+    
     for i in range(EPOCHS):
         model.train()
         yhat = model(X_train)
     
-        loss = criterion(yhat, y_train)
-        loss.backward()
+        loss_train = criterion(yhat, y_train)
+        loss_train.backward()
         
-        print(f"Itérations {i}: batch/loss/train {loss.item()}")
-        writer.add_scalar('batch/loss/train', loss, i)
+        loss_train_tab.append(loss_train.item())
+        #print(f"Itérations {i}: batch/loss/train {loss.item()}")
+        #writer.add_scalar('batch/loss/train', loss, i)
         
         optimizer.step()
         optimizer.zero_grad()
@@ -80,40 +90,71 @@ if grad_descent == "batch":
         with torch.no_grad():
             model.eval()
             yhat = model(X_test)
-            loss = criterion(yhat, y_test)
-            writer.add_scalar('batch/loss/test', loss, i)
-            print(f"Itérations {i}: batch/loss/test {loss.item()}")
+            loss_test = criterion(yhat, y_test)
+            #writer.add_scalar('batch/loss/test', loss_test, i)
+            loss_test_tab.append(loss_test.item())
+            #print(f"Itérations {i}: batch/loss/test {loss.item()}")
+            
+        writer.add_scalars('batch/loss', {'train' : loss_train, 'test' : loss_test}, i)
+            
+    print("Temps : ", time.time() - tps)
             
 elif grad_descent == "stochastic":
-    idx = torch.randperm(len(X_train))
-    t = 0
+
     
-    for i in range(EPOCHS):
-        model.train()
-        yhat = model(X_train[idx[t]])
+    tps = time.time()
+    for i in tqdm(range(EPOCHS)):
         
-        loss = criterion(yhat, y_train[idx[t]])
-        loss.backward()
+        cumul_loss = 0
+        c = 0
         
-        print(f"Itérations {i}: stoch/loss/train {loss.item()}")
-        writer.add_scalar('stoch/loss/train', loss, i)
+        for j in range(X_train.shape[0]-300):
         
-        optimizer.step()
-        optimizer.zero_grad()
+            ind = np.random.randint(X_train.shape[0])    
         
-        t += 1
-        
+            model.train()
+            yhat = model(X_train[ind])
+            
+            loss = criterion(yhat, y_train[ind])
+            loss.backward()
+            
+            cumul_loss += loss.item()
+            c += 1
+            
+            #print(f"Itérations {i}: stoch/loss/train {loss.item()}")
+            #writer.add_scalar('stoch/loss/train', loss, i)
+            
+            optimizer.step()
+            optimizer.zero_grad()
+            
+        loss_train_tab.append(cumul_loss / c)
         with torch.no_grad():
             model.eval()
             yhat = model(X_test)
-            loss = criterion(yhat, y_test)
-            writer.add_scalar('stoch/loss/test', loss, i)
-            print(f"Itérations {i}: stoch/loss/test {loss.item()}")
+            loss_test = criterion(yhat, y_test)
+            loss_test_tab.append(loss_test.item())
+            #writer.add_scalar('stoch/loss/test', loss, i)
+            #print(f"Itérations {i}: stoch/loss/test {loss.item()}")
+            
+        writer.add_scalars('stoch/loss', {'train' : cumul_loss/c, 'test' : loss_test}, i)
+            
+    print("Temps écoulé : ", time.time() - tps)
     
 elif grad_descent == "minibatch":
     
+    ind = np.arange(0, X_train.shape[0])
+    np.random.shuffle(ind)
+    
+    X_train = X_train[ind]
+    y_train = y_train[ind]
+    
+    tps = time.time()
+    
     for i in range(EPOCHS):
+        
         cumul_loss = 0
+        c = 0
+        
         for j in range(0, len(X_train) - BATCH_SIZE, BATCH_SIZE):
             model.train()
             yhat = model(X_train[j:j+BATCH_SIZE])
@@ -121,20 +162,33 @@ elif grad_descent == "minibatch":
             loss = criterion(yhat, y_train[j:j+BATCH_SIZE])
             loss.backward()
             
-            print(f"Itérations {i}: minib/loss/train {loss.item()}")
+            #print(f"Itérations {i}: minib/loss/train {loss.item()}")
             cumul_loss += loss.item()
-            
+            c += 1
             optimizer.step()
             optimizer.zero_grad()
             
-        writer.add_scalar('minib/loss/train', cumul_loss, i)
-            
+        #writer.add_scalar('minib/loss/train', cumul_loss, i)
+        loss_train_tab.append(cumul_loss / c)
         with torch.no_grad():
             model.eval()
             yhat = model(X_test)
-            loss = criterion(yhat, y_test)
-            print(f"Itérations {i}: minib/loss/test {loss.item()}")
-            writer.add_scalar('minib/loss/test', loss, i)
+            loss_test = criterion(yhat, y_test)
+            loss_test_tab.append(loss_test)
+            #print(f"Itérations {i}: minib/loss/test {loss.item()}")
+            #writer.add_scalar('minib/loss/test', loss, i)
+            
+        writer.add_scalars('minib/loss', {'train' : cumul_loss/c, 'test' : loss_test}, i)
     
-    
+    print("Temps : ", time.time() - tps)
 
+
+
+if len(loss_train_tab) > 0 and len(loss_test_tab) > 0:
+    fig, ax = plt.subplots(figsize=(10,5))
+    ax.grid()
+    ax.set(xlabel = "Nombre d'epochs", ylabel="Loss", title=f"Evolution de la loss sur {len(loss_train_tab)} epochs")
+    plt.plot(np.arange(len(loss_train_tab)), loss_train_tab, label="train")
+    plt.plot(np.arange(len(loss_test_tab)), loss_test_tab, label="test")
+    ax.legend()
+    plt.savefig(f"loss_boston_nn_{grad_descent}")   
